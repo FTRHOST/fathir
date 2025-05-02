62f2c2b9e1277e342a87324bc8fdba16d854da1f
fathironmy2 <fathironmy2@gmail.com>
2025-05-02 14:32:58 +0000

    test: delete integration server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,12 @@ import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from 'astro-icon'
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
-import node from '@astrojs/node';
+// import vercel from '@astrojs/vercel';
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
   site: 'https://fathir.vercel.app',
   base: '/',
   integrations: [
@@ -24,15 +23,4 @@ export default defineConfig({
     }),
     compress(),
   ],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          logger: {
-            warn: () => {},
-          },
-        },
-      },
-    },
-  },
 })
